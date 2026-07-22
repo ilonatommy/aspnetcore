4a66b9c15e1b13ef9a8381963fbcd2a967aa09d7
ilonatommy <itomkowicz@microsoft.com>
2026-07-22 09:58:55 +0200

    Feedback.

--- src/Components/Web/src/Virtualization/Virtualize.cs
+++ src/Components/Web/src/Virtualization/Virtualize.cs
@@ -484,7 +484,7 @@ public sealed class Virtualize<TItem> : ComponentBase, IVirtualizeJsCallbacks, I
 
             var deferAnchorRestoreClear = shouldRestore
                 && (AnchorMode == VirtualizeAnchorMode.None
-                    || (AnchorMode & VirtualizeAnchorMode.End) != 0
+                    || AnchorMode == VirtualizeAnchorMode.End
                     || ((AnchorMode & VirtualizeAnchorMode.Start) != 0 && _deferPrependAnchorClear));
             if (!deferAnchorRestoreClear)
             {
